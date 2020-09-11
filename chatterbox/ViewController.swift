c7fd5724b2af52a3ffa202b40a2142d167edcda8
Alexandr Lazarev <58854670+alex93133@users.noreply.github.com>
2020-09-11 16:55:26 +0300

    Initial Commit

---
+++ chatterbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatterbox
+//
+//  Created by Alexander Lazarev on 11.09.2020.
+//  Copyright © 2020 Alexander Lazarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
