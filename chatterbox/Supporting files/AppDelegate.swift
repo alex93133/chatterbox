ce51ebdf09d917887253aab3764431fe01152848
Alexandr Lazarev <58854670+alex93133@users.noreply.github.com>
2020-09-12 16:46:54 +0300

    New icons and logs implementation

--- chatterbox/Supporting files/AppDelegate.swift
+++ chatterbox/Supporting files/AppDelegate.swift
@@ -2,11 +2,40 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    // MARK: - App Lifecycle
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        printLogs(text: "Application has been loaded. State now is inactive: \(#function)")
         return true
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        printLogs(text: "Application change state from inactive to active: \(#function)")
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        printLogs(text: "Application change state from active to inactive: \(#function)")
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        printLogs(text: "Application change state from inactive to background: \(#function)")
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        printLogs(text: "Application change state from background to inactive: \(#function)")
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        printLogs(text: "Application change state from background and now has been suspended: \(#function)")
     }
 }
 
+/// Method print logs depends on building configuration
+public func printLogs(text: String) {
+    #if DEBUG
+    print(text)
+    #endif
+}
